a1d377e991b658f2d60283bb589bdfa5081b78ea
alexmon1989 <alex.mon1989@gmail.com>
2017-09-19 12:01:38 +0300

    feature: убрать контакты с шапки.

--- home/models.py
+++ home/models.py
@@ -14,8 +14,9 @@ class HomeSection(models.Model):
     text_1 = models.CharField(verbose_name='Текст 1', default='', max_length=255)
     text_2 = models.CharField(verbose_name='Текст 2 (тег h1)', default='', max_length=255)
     text_3 = models.CharField(verbose_name='Текст 3', default='', max_length=255)
-    text_4 = models.CharField(verbose_name='Текст 4', default='', max_length=255)
+    text_4 = models.TextField(verbose_name='Текст 4', default='')
     text_5 = models.CharField(verbose_name='Текст 5', default='', max_length=255)
+    text_6 = models.CharField(verbose_name='Текст 6', default='', max_length=255)
 
     def __str__(self):
         return 'Секция Home'
